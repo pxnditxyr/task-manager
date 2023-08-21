214b3c7464e23166242a38a2400a8ea225140a13
Yuki xD <pxnditxyr@gmail.com>
2023-08-21 00:58:19 -0400

    Initialized Project Task Manager | Nest GraphQL

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default'
+import { ItemsModule } from './items/items.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      autoSchemaFile: join( process.cwd(), 'src/schema.gql' ),
+      plugins: [
+        ApolloServerPluginLandingPageLocalDefault()
+      ]
+    }),
+    ItemsModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
